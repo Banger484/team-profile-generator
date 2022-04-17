d695bc3ca315d62c76b23b2d60756d8e01e8ff80
Bret Banger <Banger484@gmail.com>
2022-04-16 19:14:05 -0500

    commented out my index.js to explain the flow of my application

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 // Importing deppendencies
 const inquirer = require('inquirer')
-const fs = require('fs')    
+const fs = require('fs')
+
 // Importing my classes
-const Employee = require('./lib/employee')
 const Engineer = require('./lib/engineer')
 const Intern = require('./lib/intern')
 const Manager = require('./lib/manager')
 
+// Empty array to push my employee roster into
 const employees = []
 
+// Questions to gather the manager's information
 const managerQuestions = [
     {
         type: 'input',
@@ -31,6 +33,8 @@ const managerQuestions = [
         message: "Please enter the office number."
     },
 ]
+
+// Question used for logic to direct the flow through the prompts
 const addTeamMember = [
     {
         type: 'list',
@@ -39,6 +43,8 @@ const addTeamMember = [
         choices: ['Engineer', 'Intern', 'Finish']
     }
 ]
+
+// Questions to gather engineer's information
 const addEngineer = [
     {
         type: 'input',
@@ -61,6 +67,8 @@ const addEngineer = [
         message: "Please enter the engineer's github username."
     },
 ]
+
+// Questions to gather intern's information
 const addIntern = [
     {
         type: 'input',
@@ -84,29 +92,36 @@ const addIntern = [
     },
 ]
 
+// function to start the app
 function init () {
+    // starting with collecting manager information
     inquirer
     .prompt(managerQuestions)
     .then(answers => {
         const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber)
         console.log(manager);
         if(manager) {
+            // Once manager exists, pushes manager to employee array and calls newMember function
             employees.push(manager)
             newMember()
         }
     })
 }
 
+// calling init to start the app
 init()
 
+// This function will ask if they want to add a new engineer, new intern, or finish adding employees.
 function newMember () {
     inquirer
             .prompt(addTeamMember)
             .then(answers => {
+                // logic will direct the flow based on the user input
                 if(answers.addNext === 'Engineer') {
                     inquirer
                     .prompt(addEngineer)
                     .then(answers => {
+                        // creates new engineer and pushes it into employee array
                         const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github)
                         employees.push(engineer)
                         newMember()
@@ -115,6 +130,7 @@ function newMember () {
                     inquirer
                     .prompt(addIntern)
                     .then(answers => {
+                        // creates new intern and pushes it into employee array
                         const intern = new Intern(answers.name, answers.id, answers.email, answers.school)
                         employees.push(intern)
                         newMember()
